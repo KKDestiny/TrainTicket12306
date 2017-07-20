961561840b1b215c7b46d526e1709480e32e6b8e
KKDestiny <1139904786@qq.com>
2017-07-20 18:37:32 +0800

    实现网页版小工具   可以输入中文站名、日期，从而查询火车   点击结果列表还可以查看本次列车的途经站点信息

--- routes/index.js
+++ routes/index.js
@@ -12,9 +12,28 @@ var OL_TrainTickects = require('./../TrainTickects');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '火车票查询工具' });
 });
 
+
+
+// 查询火车票
+router.post('/tickect', function(req, res, next) {
+	var Config = {
+		date         : req.body.date,   		// '2017-07-24',	// 日期, 格式"yyyy-mm-dd"
+		from_station : req.body.from_station,   //  'SZQ',			// 始发站车站代码
+		end_station  : req.body.end_station,    //  'XMS',			// 终到站车站代码
+		print        : false,					// 是否打印
+	};
+	OL_TrainTickects.QueryTickects(Config, function(err, tickects) {
+		if(!err) {
+			res.json({success:"SUCCESS", data:tickects})
+		}
+		else {
+			res.json({error:err})
+		}
+	});
+});
 
 
 // 更新火车站数据文件
@@ -36,6 +55,25 @@ router.post('/rebase', function(req, res, next) {
 		}
 		
 	})
+});
+
+// 查询途径站点
+router.post('/stations', function(req, res, next) {
+	var Config = {
+		train_no     : req.body.train_no,   	// '6i000D40920A',	// 列车编号
+		from_station : req.body.from_station,   //  'SZQ',			// 始发站车站代码
+		end_station  : req.body.end_station,    //  'XMS',			// 终到站车站代码
+		date         : req.body.date,   		// '2017-07-24',	// 日期, 格式"yyyy-mm-dd"
+		print        : false,					// 是否打印
+	};
+	OL_TrainTickects.QueryStations(Config, function(err, sList) {
+		if(!err) {
+			res.json({success:"SUCCESS", data:sList})
+		}
+		else {
+			res.json({error:err})
+		}
+	});
 });
 
 
